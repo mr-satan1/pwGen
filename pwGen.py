367e46549a8cb7d1134eef70ead4d098fd125615
Davide D'Agostino <daddye@uber.com>
2016-04-29 13:09:59 -0700

    clean up code slightly.

--- pwGen.py
+++ pwGen.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 # Example of simple multiprocessing
 
 import multiprocessing as mp
@@ -13,11 +12,11 @@ outputQueue = mp.Queue()
 # Define a function to process.
 # In this case, lets generate a random 15 character passphrase.
 def randomString(length, outputQueue):
-    stringy = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation) for _ in range(15))
+    stringy = ''.join(random.choice(string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation) for _ in range(length))
     outputQueue.put(stringy)
 
 # Set up a list of processes that we want to run
-processes = [mp.Process(target=randomString, args=(5, outputQueue)) for x in range(4)]
+processes = [mp.Process(target=randomString, args=(15, outputQueue)) for x in range(10)]
 
 # Execute processes 
 for process in processes:
@@ -32,4 +31,4 @@ for process in processes:
 results = [outputQueue.get() for process in processes]
 os.system('clear')
 print "Here's some passwords:\n"
-print results
+print "\n".join(results)
